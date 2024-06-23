6d01c1764044207bd86556cbd467b71ba79fe02e
Alex Locher <builderofscrap@gmail.com>
2024-06-22 20:51:38 -0400

    things done in class of 6/20/2024

--- gun.py
+++ gun.py
@@ -4,12 +4,14 @@ import time
 
 
 class Gun:
-    def __init__(self, screen, x, lmb):
+    def __init__(self, screen, x, lmb,):
         self.screen = screen
         self.x = x
         self.lmb = 0
+        self.xnew = 0
         self.angle = 0
         self.shooting = False
+        self.recoil = 0
         # self.image = pygame.image.load("sprites/gun/aim/aim (1).png")
         self.images = []
         self.start = 20
@@ -21,25 +23,33 @@ class Gun:
     def shoot(self):
         if self.start < 5:
             self.lmb = 1
-        if self.start > 5:
+        if self.start < 5 and self.recoil < 30:
+            self.recoil = self.recoil + 10
+        if self.start > 2:
             self.lmb = 0
         self.start = self.start + 1
+        if self.recoil > 0 and self.start > 5:
+            self.recoil = self.recoil - 10
+
 
     def location(self):
+
        if pygame.mouse.get_pos()[0] < 200:
            self.angle = 1
        elif pygame.mouse.get_pos()[0] > 600:
            self.angle = 2
        else:
            self.angle = 0
+
+       self.xnew = (pygame.mouse.get_pos()[0] - 400) / 20
+
     def draw(self):
         if self.lmb == 1:
             fire = 3
         else:
             fire = 0
         image_index = self.angle + fire
-        self.screen.blit(self.images[image_index], (self.x, 0))
-        print(self.start)
+        self.screen.blit(self.images[image_index], (self.xnew, self.recoil))
 def main():
     pygame.init()
     pygame.display.set_caption("gun test")
@@ -54,7 +64,6 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 gun.lmb = 1
                 gun.start = 0
-
         gun.shoot()
         screen.fill((255, 255, 255))
         gun.location()
